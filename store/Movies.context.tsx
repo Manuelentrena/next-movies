6f7ea6a1baf74812b83bcf314c524f1d0e94ae49
EntrenaDev <manuel.entrena@gmail.com>
2024-10-01 18:18:51 +0200

    refactor: :recycle: Change state in Movie Context

--- store/Movies.context.tsx
+++ store/Movies.context.tsx
@@ -1,25 +1,22 @@
 import { MOVIE_SEARCH_BY_DEFAULT, PAGE_BY_DEFAULT, TYPE_BY_DEFAULT } from "@/config/initial";
 import { GetMovies, MovieRepository } from "@/core/movies/domain/contract/MovieRepository";
-import { MovieList, TypesMovie } from "@/core/movies/domain/Movie";
+import { Movie, TypesMovie } from "@/core/movies/domain/Movie";
 import { createContext, useCallback, useContext, useEffect, useRef, useState } from "react";
 
 export interface MoviesContextState {
-  movies: MovieList;
+  movies: Movie[];
+  total: number;
   getMovies: ({ title, type, page }: GetMovies) => Promise<void>;
   currentPage: number;
   currentTitle: string;
   currentType: string;
 }
 
-const initialMovieList: MovieList = {
-  Movies: [],
-  Total: "0",
-};
-
 export const MoviesContext = createContext({} as MoviesContextState);
 
 export const MoviesContextProvider = ({ children, service }: React.PropsWithChildren<{ service: MovieRepository }>) => {
-  const [movies, setMovies] = useState<MovieList>(initialMovieList);
+  const [movies, setMovies] = useState<Movie[]>([]);
+  const [total, setTotal] = useState<number>(0);
   const [title, setTitle] = useState<string>(MOVIE_SEARCH_BY_DEFAULT);
   const [type, setType] = useState<TypesMovie>(TYPE_BY_DEFAULT);
   const [page, setPage] = useState<number>(PAGE_BY_DEFAULT);
@@ -35,8 +32,9 @@ export const MoviesContextProvider = ({ children, service }: React.PropsWithChil
 
   const getMovies = useCallback(
     async ({ title, type, page }: GetMovies) => {
-      const movies = await service.getMovies({ title, type, page });
-      setMovies(movies);
+      const moviesList = await service.getMovies({ title, type, page });
+      setMovies(moviesList.Movies);
+      setTotal(Number(moviesList.Total));
       setTitle(title);
       setType(type);
       setPage(page);
@@ -48,6 +46,7 @@ export const MoviesContextProvider = ({ children, service }: React.PropsWithChil
     <MoviesContext.Provider
       value={{
         movies,
+        total,
         getMovies,
         currentPage: page,
         currentTitle: title,
