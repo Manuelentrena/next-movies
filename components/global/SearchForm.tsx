ce95b989afb3654d215fd8b511c709ad63f7cf9b
EntrenaDev <manuel.entrena@gmail.com>
2024-10-01 19:41:47 +0200

    build: :building_construction: Moved component search to another place, called SearchForm

--- components/global/SearchForm.tsx
+++ components/global/SearchForm.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import { Form, FormControl, FormField, FormItem, FormMessage } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import {
   Select,
@@ -32,7 +32,7 @@ interface SearchProps {
   getMovies: (values: GetMovies) => void;
 }
 
-export function Search({ getMovies }: SearchProps) {
+export function SearchForm({ getMovies }: SearchProps) {
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
@@ -47,13 +47,12 @@ export function Search({ getMovies }: SearchProps) {
 
   return (
     <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+      <form onSubmit={form.handleSubmit(onSubmit)} className="my-10 flex items-center justify-center space-x-4">
         <FormField
           control={form.control}
           name="title"
           render={({ field }) => (
             <FormItem>
-              <FormLabel>Title</FormLabel>
               <FormControl>
                 <Input placeholder="title" {...field} />
               </FormControl>
@@ -67,7 +66,6 @@ export function Search({ getMovies }: SearchProps) {
           name="type"
           render={({ field }) => (
             <FormItem>
-              <FormLabel>Type</FormLabel>
               <FormControl>
                 <Select onValueChange={field.onChange} defaultValue={field.value}>
                   <SelectTrigger className="w-[180px]">
@@ -90,7 +88,9 @@ export function Search({ getMovies }: SearchProps) {
           )}
         />
 
-        <Button type="submit">Submit</Button>
+        <Button type="submit" className="flex justify-end">
+          Buscar
+        </Button>
       </form>
     </Form>
   );
