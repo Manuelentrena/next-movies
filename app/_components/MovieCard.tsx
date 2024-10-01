7c3496b784dbf4b72da0982201cf3365dfb2f523
EntrenaDev <manuel.entrena@gmail.com>
2024-10-01 23:33:28 +0200

    style: :art: Added start to movieCard

--- app/_components/MovieCard.tsx
+++ app/_components/MovieCard.tsx
@@ -1,8 +1,11 @@
 "use client";
 
+import { Badge } from "@/components/ui/badge";
 import { Movie } from "@/core/movies/domain/Movie";
 import { cn } from "@/utils/utils";
-import { memo } from "react";
+import { Star } from "lucide-react";
+import { memo, useState } from "react";
+
 
 interface MovieCardProps extends Omit<Movie, "Id"> {
   index: number;
@@ -11,6 +14,12 @@ interface MovieCardProps extends Omit<Movie, "Id"> {
 }
 
 const MovieCard: React.FC<MovieCardProps> = memo(({ Title, Year, Type, Poster, hovered, index, setHovered }) => {
+  const [isFavorite, setIsFavorite] = useState(false);
+
+  const handleStarClick = () => {
+    setIsFavorite(!isFavorite);
+  };
+
   return (
     <div
       onMouseEnter={() => setHovered(index)}
@@ -20,21 +29,31 @@ const MovieCard: React.FC<MovieCardProps> = memo(({ Title, Year, Type, Poster, h
         hovered !== null && hovered !== index && "scale-[0.98] blur-sm",
       )}
     >
+      {/* Image */}
       {Poster ? (
         <img src={Poster} alt={`${Title} Poster`} className="absolute inset-0 h-full w-full object-cover" />
       ) : null}
+
+      {/* Star */}
+      <div className="absolute top-2 right-2 cursor-pointer z-10" onClick={handleStarClick}>
+        {isFavorite ? (
+          <Star fill="yellow" strokeWidth={0} className="h-8 w-8 relative " />
+        ) : (
+          <Star className="h-8 w-8 text-yellow-100 relative " />
+        )}
+      </div>
+      {/* Background */}
       <div
         className={cn(
-          "absolute inset-0 flex cursor-pointer items-end bg-black/50 px-4 py-8 transition-opacity duration-300",
+          "absolute inset-0 flex cursor-pointer items-end bg-black/70 px-4 py-8 transition-opacity duration-300",
           hovered === index ? "opacity-100" : "opacity-0",
         )}
       >
-        <div className="bg-gradient-to-b from-neutral-50 to-neutral-200 bg-clip-text text-xl font-medium text-transparent sm:text-lg md:text-2xl">
+        <div>
           <div className="flex flex-col">
-            <span>{Title}</span>
+            <span className="bg-gradient-to-b from-neutral-50 to-neutral-200 bg-clip-text text-xl font-extrabold text-transparent sm:text-lg md:text-2xl">{Title} ({Year})</span>
             <span className="flex">
-              {Type}
-              {Year}
+              <Badge variant="outline" className="mt-2 bg-primary text-white">{Type}</Badge>
             </span>
           </div>
         </div>
