ec36d7f6e92f97e640c34396e6ba04c5ff617f50
EntrenaDev <manuel.entrena@gmail.com>
2024-10-01 11:20:50 +0200

    chore:

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,14 @@
+import { BASE_URL } from "config/env";
+
 export const metadata = {
   title: "App Router",
 };
 
 export default function Page() {
-  return <h1>App Router</h1>;
+  return (
+    <>
+      <h1>App Router</h1>
+      <p>{BASE_URL}</p>
+    </>
+  );
 }
