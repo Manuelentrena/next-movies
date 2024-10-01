8a8c8782fdddbbdb379d35beacd6da7367844d0a
EntrenaDev <manuel.entrena@gmail.com>
2024-10-01 21:32:18 +0200

    style: :art: Improved styles of the grid and card movies

--- app/_components/MovieCard.tsx
+++ app/_components/MovieCard.tsx
@@ -16,7 +16,7 @@ const MovieCard: React.FC<MovieCardProps> = memo(({ Title, Year, Type, Poster, h
       onMouseEnter={() => setHovered(index)}
       onMouseLeave={() => setHovered(null)}
       className={cn(
-        "relative h-96 w-full overflow-hidden rounded-lg bg-gray-100 transition-all duration-300 ease-out md:h-96 dark:bg-neutral-900",
+        "relative h-60 w-full overflow-hidden rounded-lg bg-gray-100 transition-all duration-300 ease-out md:h-96 dark:bg-neutral-900",
         hovered !== null && hovered !== index && "scale-[0.98] blur-sm",
       )}
     >
@@ -25,14 +25,18 @@ const MovieCard: React.FC<MovieCardProps> = memo(({ Title, Year, Type, Poster, h
       ) : null}
       <div
         className={cn(
-          "absolute inset-0 flex items-end bg-black/50 px-4 py-8 transition-opacity duration-300",
+          "absolute inset-0 flex cursor-pointer items-end bg-black/50 px-4 py-8 transition-opacity duration-300",
           hovered === index ? "opacity-100" : "opacity-0",
         )}
       >
-        <div className="bg-gradient-to-b from-neutral-50 to-neutral-200 bg-clip-text text-xl font-medium text-transparent md:text-2xl">
-          {Title}
-          {Year}
-          {Type}
+        <div className="bg-gradient-to-b from-neutral-50 to-neutral-200 bg-clip-text text-xl font-medium text-transparent sm:text-lg md:text-2xl">
+          <div className="flex flex-col">
+            <span>{Title}</span>
+            <span className="flex">
+              {Type}
+              {Year}
+            </span>
+          </div>
         </div>
       </div>
     </div>
