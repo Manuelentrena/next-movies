aa52eaf0f2b1688cd7d77c3de737822240512d5b
EntrenaDev <manuel.entrena@gmail.com>
2024-10-04 13:16:42 +0200

    style: :art: Improved style header and container movies

--- components/global/Header.tsx
+++ components/global/Header.tsx
@@ -1,12 +1,17 @@
+import { useRouter } from "next/navigation";
+
 export function Header() {
+  const router = useRouter();
   return (
     <header className="bg-primary bg-gradient-to-r to-secondary text-white shadow-lg">
       <div className="container relative mx-auto flex h-24 max-w-5xl items-center justify-between px-8">
-        <img
-          src="/ticket.webp"
-          alt="Logo"
-          className="absolute left-1/2 top-1/2 h-auto max-w-44 -translate-x-1/2 -translate-y-1/2 transform"
-        />
+        <div className="cursor-pointer overflow-hidden" onClick={() => router.push("/")}>
+          <img
+            src="/ticket.webp"
+            alt="Logo Movies"
+            className="absolute left-1/2 top-1/2 h-auto max-w-44 -translate-x-1/2 -translate-y-1/2 transform transition-transform duration-300 hover:scale-105"
+          />
+        </div>
       </div>
     </header>
   );
