08d739cf9eef4decae02fd748b2508c705d390e2
EntrenaDev <manuel.entrena@gmail.com>
2024-10-13 23:06:13 +0200

    test: :test_tube: Modified config jest

--- jest.config.js
+++ jest.config.js
@@ -9,7 +9,8 @@ const createJestConfig = nextJest({
 const customJestConfig = {
   setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
   testEnvironment: "jsdom",
-  testPathIgnorePatterns: ["/node_modules/", "/__mocks__/", "/core/*"],
+  testPathIgnorePatterns: ["/node_modules/", "/__mocks__/", "/core/*", "/utis/*"],
+  testMatch: ["**/*.test.tsx"],
 };
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
