45566d4fe5d0059ed069f36fac5b159176e8723a
EntrenaDev <manuel.entrena@gmail.com>
2024-10-13 23:09:56 +0200

    test: :test_tube: Modified MovieMother

--- __tests__/core/movies/domain/MovieMother.ts
+++ __tests__/core/movies/domain/MovieMother.ts
@@ -1,32 +1,52 @@
-import { getRandomGenres, MovieRating } from "@/__tests__/__mocks__/randomElements";
+import { getRandomGenres, MovieRating } from "@/__tests__/utils/randomElements";
 import { Movie, MovieDetail, TypesMovie } from "@/core/movies/domain/Movie";
 import { faker } from "@faker-js/faker";
 import { Factory } from "fishery";
 
-export const MovieFactory = Factory.define<Movie>(() => ({
-  Title: faker.lorem.words(),
-  Year: faker.date.between({ from: "1900-01-01", to: "2024-12-31" }).getFullYear().toString(),
-  Id: faker.string.uuid(),
-  Poster: faker.image.url(),
-  Fav: faker.datatype.boolean(),
-  Type: faker.helpers.enumValue(TypesMovie),
-}));
+export const MovieFactory = Factory.define<Movie>(({ sequence, afterBuild }) => {
+  afterBuild((movie) => {
+    const movieType =
+      movie.Type === TypesMovie.ALL
+        ? faker.helpers.arrayElement([TypesMovie.MOVIE, TypesMovie.SERIES, TypesMovie.GAME])
+        : movie.Type;
 
-export const MovieDetailFactory = Factory.define<MovieDetail>(() => ({
-  Title: faker.lorem.words(),
-  Year: faker.date.between({ from: "1900-01-01", to: "2024-12-31" }).getFullYear().toString(),
-  Id: faker.string.uuid(),
-  Poster: faker.image.url(),
-  Fav: faker.datatype.boolean(),
-  Type: faker.helpers.enumValue(TypesMovie),
-  Rated: faker.helpers.enumValue(MovieRating),
-  Released: faker.date.past().toLocaleDateString("en-US", { year: "2-digit", month: "short", day: "numeric" }),
-  Runtime: `${faker.number.bigInt({ min: 1, max: 300 })} min`,
-  Genre: getRandomGenres(),
-  Director: faker.lorem.words(2),
-  Score: faker.number.int({ min: 0, max: 100 }).toString(),
-  Plot: faker.lorem.paragraph(),
-}));
+    movie.Type = movieType;
+  });
+  return {
+    Title: faker.lorem.words(),
+    Year: faker.date.between({ from: "1900-01-01", to: "2024-12-31" }).getFullYear().toString(),
+    Id: sequence.toString(),
+    Poster: faker.image.url(),
+    Fav: false,
+    Type: faker.helpers.arrayElement([TypesMovie.MOVIE, TypesMovie.SERIES, TypesMovie.GAME]),
+  };
+});
+
+export const MovieDetailFactory = Factory.define<MovieDetail>(({ sequence, afterBuild }) => {
+  afterBuild((movie) => {
+    const movieType =
+      movie.Type === TypesMovie.ALL
+        ? faker.helpers.arrayElement([TypesMovie.MOVIE, TypesMovie.SERIES, TypesMovie.GAME])
+        : movie.Type;
+
+    movie.Type = movieType;
+  });
+  return {
+    Title: faker.lorem.words(),
+    Year: faker.date.between({ from: "1900-01-01", to: "2024-12-31" }).getFullYear().toString(),
+    Id: sequence.toString(),
+    Poster: faker.image.url(),
+    Fav: false,
+    Type: faker.helpers.arrayElement([TypesMovie.MOVIE, TypesMovie.SERIES, TypesMovie.GAME]),
+    Rated: faker.helpers.enumValue(MovieRating),
+    Released: faker.date.past().toLocaleDateString("en-US", { year: "2-digit", month: "short", day: "numeric" }),
+    Runtime: `${faker.number.bigInt({ min: 1, max: 300 })} min`,
+    Genre: getRandomGenres(),
+    Director: faker.lorem.words(2),
+    Score: faker.number.int({ min: 0, max: 100 }).toString(),
+    Plot: faker.lorem.paragraph(),
+  };
+});
 
 export const MovieMother = {
   create: (params?: Partial<Movie>): Movie => {
